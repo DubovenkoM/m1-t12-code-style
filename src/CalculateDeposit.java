2d315607d5f4ccc5722d183c6e6cba4e1c399c57
RufatYagofarov <rufat.yagofarov@yandex.ru>
2022-07-24 18:49:43 +0300

    Пурум-пум-пум

---
+++ src/CalculateDeposit.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class CalculateDeposit {
+    double calculateComplexPercent(double amount, int depositPeriodInYear) {
+        return Math.round(amount * Math.pow((1 + 0.06 / 12), 12 * depositPeriodInYear));
+    }
+
+    double calculateSimplePercent(double amount,  int depositPeriodInYear) {
+        return Math.round(amount + amount * 0.06 * depositPeriodInYear);
+    }
+
+    void percentManager() {
+        int period;
+        int action;
+        int amount;
+        double totalSum = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите сумму вклада в рублях:");
+        amount = scanner.nextInt();
+
+        System.out.println("Введите срок вклада в годах:");
+        period = scanner.nextInt();
+
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        action = scanner.nextInt();
+
+        if (action ==1)
+            totalSum = calculateSimplePercent(amount, period);
+        else if (action == 2)
+            totalSum = calculateComplexPercent(amount, period);
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + totalSum);
+    }
+    
+    public static void main(String[] args) {
+        new CalculateDeposit().percentManager();
+    }
+}
